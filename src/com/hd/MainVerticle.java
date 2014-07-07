38d89672f630096be8bfd3d1340a03bcf986d569
zhhzhfya <zhhzhfya@163.com>
2014-07-07 15:43:46 +0800

    modify...

---
+++ src/com/hd/MainVerticle.java
@@ -0,0 +1,9 @@
+package com.hd;
+
+import org.vertx.java.platform.Verticle;
+
+public class MainVerticle extends Verticle {
+	public void start() {
+		
+	}
+}
--- src/http/ServerBase.java
+++ src/http/ServerBase.java
@@ -1,20 +1,21 @@
 package http;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.LinkOption;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashMap;
-import java.util.Scanner;
 
 import org.vertx.java.core.AsyncResult;
 import org.vertx.java.core.Handler;
+import org.vertx.java.core.eventbus.Message;
 import org.vertx.java.core.json.JsonArray;
 import org.vertx.java.core.json.JsonObject;
 import org.vertx.java.core.logging.Logger;
 import org.vertx.java.platform.Verticle;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.hd.PingVerticle;
 
 import frame.MainFrame;
 
@@ -25,6 +26,50 @@ public class ServerBase extends Verticle {
 		final Logger logger = container.logger();
 		JsonObject appConfig = container.config();
 		JsonArray deployModules = appConfig.getArray("deploy_modules");
+		// 发布模块
+		deployModules(logger, deployModules);
+		// 一共监控界面
+		MainFrame frame = new MainFrame(vertx.eventBus());
+		frame.setVisible(true);
+		container.deployVerticle(PingVerticle.class.getName());
+		
+		try {
+			Thread.sleep(200);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		BufferedReader reader = null;
+		try {
+			File file = new File("E:\\work\\workspace_vertx\\vx_platform.git\\zips.json0");
+			reader = new BufferedReader(new FileReader(file));
+			String tempString = null;
+			while ((tempString = reader.readLine()) != null) {
+				JsonObject m = new JsonObject();
+				m.putString("action", "save");
+				m.putString("collection", "mydb");
+				m.putObject("document", new JsonObject(tempString));
+				vertx.eventBus().send("hd.mgo", m, new Handler<Message<JsonObject>>() {
+					@Override
+					public void handle(Message<JsonObject> event) {
+						System.out.println(event.body().toString());
+					}
+				});
+			}
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e1) {
+				}
+			}
+		}
+	}
+
+	private void deployModules(final Logger logger, JsonArray deployModules) {
 		for (int i = 0; i < deployModules.size(); i++) {
 			JsonObject module = deployModules.get(i);
 			JsonObject config = module.getObject("config", new JsonObject());
@@ -45,7 +90,33 @@ public class ServerBase extends Verticle {
 				});
 			}
 		}
-		MainFrame frame = new MainFrame(vertx.eventBus());
-		frame.setVisible(true);
 	}
+
+	public static void readFileByLines(String fileName) {
+		File file = new File(fileName);
+		BufferedReader reader = null;
+		try {
+			System.out.println("以行为单位读取文件内容，一次读一行");
+			reader = new BufferedReader(new FileReader(file));
+			String tempString = null;
+			int line = 1;
+			// 一次读一行，读入null时文件结束
+			while ((tempString = reader.readLine()) != null) {
+				// 把当前行号显示出来
+				System.out.println("line " + line + ": " + tempString);
+				line++;
+			}
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e1) {
+				}
+			}
+		}
+	}
+
 }
